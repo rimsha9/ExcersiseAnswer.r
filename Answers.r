f18fcb25bb02d34cec5b0a8269ceea4ddf14552b
rimsha9 <krimshah9@hotmail.com>
2021-02-09 18:14:22 +0500

    Assignment answers part 1 (answer2 remaining)

--- Answers.r
+++ Answers.r
@@ -1,3 +1,14 @@
 # New Assignment
 
+x <- c("R","I","M","S","H","A","H")                         #Answer1
+x
+
+y <- 100:120                                                #Answer3
+y
+
+m <- matrix(seq.int(2,18,2), nrow=3, ncol=3)                #Answer4
+m
+
+dimnames(m) <- list(c("r1","r2","r3"), c("c1","c2","c3"))   #Answer5
+m
 
